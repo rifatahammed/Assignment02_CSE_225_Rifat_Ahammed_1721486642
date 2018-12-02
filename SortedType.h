bcbff80d8cf1f3dbe5f10c5875b88ecb9eb625b3
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-03 02:46:11 +0600

    Sorted Type Link List from Lab 6

--- SortedType.h
+++ SortedType.h
@@ -1,1 +1,34 @@
 
+//from Lab 6
+#ifndef SORTEDTYPE_H_INCLUDED
+#define SORTEDTYPE_H_INCLUDED
+
+template<class ItemType>
+class SortedType
+{
+struct NodeType
+{
+ItemType info;
+NodeType*next;
+};
+public:
+     SortedType();
+    ~ SortedType();
+    bool IsFull();
+    int GetLength();
+    void MakeEmpty();
+    ItemType GetItem(ItemType,bool&);
+    bool PutItem(ItemType);
+    bool DeleteItem(ItemType);
+    void ResetList();
+    ItemType GetNextItem();
+    void print();
+private:
+    int length = 0;
+    NodeType * listData;
+    NodeType *currentPos;
+
+};
+#endif // UNSORTEDTYPE_H_INCLUDED
+
+
