ad8b226e0e135d926cc133bab0ff3d243c7503c4
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-03 03:05:24 +0600

    Update driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include"Customer.h"
+#include"Person.h"
+#include"PhoneNumber.h"
+#include"SortedType.h"
+#include"UnsortedType.h"
+
+using namespace std;
+
+int main()
+{
+SortedType<int> dataBase;
+cout<<"Creating two customer"<<endl;
+Customer c1;
+Customer c2;
