65461d3113bfdcf2fde2c2f4a2047036f70518d7
Rifat Ahammed <36264309+rifatahammed@users.noreply.github.com>
2018-12-03 02:02:01 +0600

    Customer.cpp added

---
+++ Customer.cpp
@@ -0,0 +1,20 @@
+#include "Customer.h"
+Customer(std::string& name,int age){
+    name=name;
+    age=age;
+}
+~Customer();
+void Customer:: set_number(int operatorNo,std::string& number){
+    operatorNo=operatorNo;
+    number=number;
+}
+std::string Customer::get_number(){
+return number;
+}
+	int Customer::get_operator(){
+	return operatorNo;
+	}
+
+	void Customer::change_operator(int newOperatorNo,std::string& number){
+        operatorNo=newOperatorNo;
+	}
